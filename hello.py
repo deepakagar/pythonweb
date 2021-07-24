a40aaa509f339c6d26cc9df8289b00862ee700e0
Deepak Agar <deepakagar2000@gmail.com>
2021-07-24 08:42:25 -0500

    commit in dev branch

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-        return "Hello World1234567"
+        return "Hello Dev"
 if __name__ == "__main__":
     app.run()
