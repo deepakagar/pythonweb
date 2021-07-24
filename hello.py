42ba8800426cdf92fea6ddcf49291a2f8acc8feb
Deepak Agar <deepakagar2000@gmail.com>
2021-07-24 14:30:13 -0500

    commit 6

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-        return "Hello World123"
+        return "Hello COMMIT6"
 if __name__ == "__main__":
     app.run()
